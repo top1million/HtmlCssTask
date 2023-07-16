b421920b21c0d6a2b6254a1393d913f03ae7b417
top1million <1180620@student.birzeit.edu>
2023-07-16 16:52:30 +0300

    hello

--- assets/javescript/script.js
+++ assets/javescript/script.js
@@ -1,14 +1,26 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel();
-  });
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    margin: 40,
-    nav: true,
-    autoWidth: false,
-    items: 3,
-    navText: [
-      "<span><i class='fa-solid fa-arrow-left-long fa-lg'></i></span>",
-      "<span><i class='fa-solid fa-arrow-right-long fa-lg'></i></span>",
-    ],
-  });
+  $(".owl-carousel").owlCarousel();
+});
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 40,
+  nav: true,
+  autoWidth: false,
+  items: 3,
+  navText: [
+    "<span><i class='fa-solid fa-arrow-left-long fa-lg'></i></span>",
+    "<span><i class='fa-solid fa-arrow-right-long fa-lg'></i></span>",
+  ],
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+      dots: true,
+    },
+    600: {
+      items: 2,
+      nav: false,
+      dots: true,
+    },
+  },
+});
